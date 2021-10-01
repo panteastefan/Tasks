b6648f0fd22a7ec21e7b4824e56085e1724ea6e5
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-02 00:40:40 +0300

    Fixed multiple errors

--- src/main/java/com/application/Tasks/TasksApplication.java
+++ src/main/java/com/application/Tasks/TasksApplication.java
@@ -2,9 +2,8 @@ package com.application.Tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class TasksApplication {
 
 	public static void main(String[] args) {
