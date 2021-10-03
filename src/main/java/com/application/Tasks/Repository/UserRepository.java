2434f18710d7cd01e6423c3fe5c85fa7586d5e36
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-03 22:49:57 +0300

    Added findUserByUsernameAndPassword

--- src/main/java/com/application/Tasks/Repository/UserRepository.java
+++ src/main/java/com/application/Tasks/Repository/UserRepository.java
@@ -11,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     void deleteUserById(Long id);
     Optional<User> findByUserName(String userName);
     User findUserById(Long id);
+    User findUserByUserNameAndPassword(String userName, String password);
 }
